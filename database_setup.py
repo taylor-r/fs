014e5343141bd49b7fd668dd8b8f513c69c36b1c
truss88 <truss88@gmail.com>
2015-11-22 01:08:20 -0500

    fixed typo

--- database_setup.py
+++ database_setup.py
@@ -25,7 +25,7 @@ class MenuItem(Base):
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     course = Column(String(250))
-    description = Column(Sring(250))
+    description = Column(String(250))
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
